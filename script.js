995b455501c5bd500e265012486504ad78400b2b
jvanheeswijck <67222238+jvanheeswijck@users.noreply.github.com>
2020-07-16 14:29:16 +0930

    Added extra cells

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
 const WINNING_COMBINATIONS = [
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
-    [0, 4, 8, 12],
-    [1, 5, 9, 13],
-    [2, 6, 10, 14],
-    [3, 7, 11, 15],
-    [0, 5, 10, 15],
-    [3, 6, 9, 12]   
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24],
+    [0, 5, 10, 15, 20],
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24],
+    [0, 6, 12, 18, 24],
+    [4, 8, 12, 16, 20]   
 ];
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
@@ -85,7 +87,6 @@ function showEndGameScreen() {
 
 function showMainMenu() {
     winningMessageElement.classList.add("show");
-    undoButton.classList.remove("show")
 }
 
 function setMainMenuElements() {
@@ -96,22 +97,26 @@ function setMainMenuElements() {
         peaceMessageTextElement.innerText = ('Peace');
         takeOverButton.classList.add("show");
         restartButton.classList.add("show")
+        undoButton.classList.remove("show")
     } else if (isDraw()) {
         winningMessageTextElement.innerText = ('Draw!');
         losingMessageTextElement.innerText = ('');
         peaceMessageTextElement.innerText = ('Peace');
-        takeOverButton.classList.remove("show");
-        restartButton.classList.remove("show")
+        restartButton.classList.add("show")
+        undoButton.classList.remove("show")
     } else if (gameInProgress()) {
         winningMessageTextElement.innerText = ('Game still in progress')
         losingMessageTextElement.innerText = ('')
         peaceMessageTextElement.innerText = ('Peace')
         restartButton.classList.remove("show")
+        undoButton.classList.add("show")
     } else {
         winningMessageTextElement.innerText = ('Game started')
         losingMessageTextElement.innerText = ('')
         peaceMessageTextElement.innerText = ('Peace')
+        takeOverButton.classList.remove("show")
         restartButton.classList.remove("show")
+        undoButton.classList.add("show")
     }
 }
 
